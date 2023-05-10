dc4bea229ce0c077c20ee468d52287b3a31e283e
Erick <frederick.christian@madeiraresearch.com>
2023-05-10 10:29:14 +0700

    add get Element utk ambil 1 element

--- model/ModelTheme.php
+++ model/ModelTheme.php
@@ -13,6 +13,7 @@ require_once __DIR__ . '/../library/Log.php';
 class ModelTheme
 {
 	const SQL_GET = 'SELECT * FROM vtheme_element WHERE theme_id=?';
+	const SQL_GET_ELEMENT = 'SELECT * FROM vtheme_element WHERE theme_id=? AND element_id=?';
 
 	public static function get($themeId){
 
@@ -23,6 +24,31 @@ class ModelTheme
 
 			$rows = $stmt->fetchAll();
 			return $rows;
+
+		}catch (PDOException $e){
+			Log::writeErrorLn($e->getMessage());
+			return $e;
+		}
+	}
+
+    /**
+     * Ambil hanya 1 theme saja
+     *
+     * @param $themeId
+     * @param $elementId
+     * @return Exception|int|PDOException
+     */
+	public static function getElement($themeId, $elementId){
+
+		try{
+			$pdo = Koneksi::create();
+			$stmt = $pdo->prepare(ModelTheme::SQL_GET_ONE);
+			$stmt->execute( [ $themeId, $elementId] );
+
+			$rows = $stmt->fetchAll();
+
+			if (sizeof($rows)==0) return 0;
+			return $rows[0];
 
 		}catch (PDOException $e){
 			Log::writeErrorLn($e->getMessage());
