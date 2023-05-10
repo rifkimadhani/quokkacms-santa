87eaaef55f371807e6f0af4380e214c179285118
rifkimadhani <rifkimadhani@gmail.com>
2023-05-10 11:19:36 +0700

    update - view Room (done)

--- app/Models/RoomForm.php
+++ app/Models/RoomForm.php
@@ -13,26 +13,27 @@ class RoomForm extends BaseForm
     public $location;
     public $room_type_id;
     public $theme_id;
-    public $package_id;
-    public $subscriber_id;
-    public $create_date;
-    public $update_date;
+    // public $package_id;
+    // public $subscriber_id;
     public $status;
     public $security_pin;
 
+    const STATUS = [
+        ['id'=>'VACANT', 'value'=>'VACANT'],
+        ['id'=>'OCCUPIED', 'value'=>'OCCUPIED']
+    ];
 
-    function __construct()
+
+    function __construct($type=[], $theme=[])
     {
-        $this->room_id = ['type'=>'numeric', 'label'=>'Room Id', 'placeholder'=>'', 'required'=>''];
+        $this->room_id = ['type'=>'numeric', 'label'=>'Room Id', 'readonly'=>'readonly'];
         $this->name = ['type'=>'varchar', 'label'=>'Name', 'placeholder'=>'', 'required'=>''];
         $this->location = ['type'=>'varchar', 'label'=>'Location', 'placeholder'=>'', 'required'=>''];
-        $this->room_type_id = ['type'=>'numeric', 'label'=>'Room Type Id', 'placeholder'=>'', 'required'=>''];
-        $this->theme_id = ['type'=>'numeric', 'label'=>'Theme Id', 'placeholder'=>'', 'required'=>''];
-        $this->package_id = ['type'=>'numeric', 'label'=>'Package Id', 'placeholder'=>'', 'required'=>''];
-        $this->subscriber_id = ['type'=>'numeric', 'label'=>'Subscriber Id', 'placeholder'=>'', 'required'=>''];
-        $this->create_date = ['type'=>'datetime', 'label'=>'Create Date', 'placeholder'=>'', 'required'=>''];
-        $this->update_date = ['type'=>'datetime', 'label'=>'Update Date', 'placeholder'=>'', 'required'=>''];
-        $this->status = ['type'=>'varchar', 'label'=>'Status', 'placeholder'=>'', 'required'=>''];
+        $this->room_type_id = ['type'=>'select', 'label'=>'Room Type', 'options'=>$type, 'placeholder'=>'Select Room Type'];
+        $this->theme_id = ['type'=>'select','label'=>'Theme', 'options'=>$theme, 'placeholder'=>'Select Theme'];
+        // $this->package_id = ['type'=>'numeric', 'label'=>'Package Id', 'placeholder'=>'', 'required'=>''];
+        // $this->subscriber_id = ['type'=>'numeric', 'label'=>'Subscriber Id', 'placeholder'=>'', 'required'=>''];
+        $this->status = ['type'=>'select', 'label'=>'Status', 'options'=>self::STATUS,'placeholder'=>'Select Room Status', 'required'=>''];
         $this->security_pin = ['type'=>'varchar', 'label'=>'Security Pin', 'placeholder'=>'', 'required'=>''];
 
     }
