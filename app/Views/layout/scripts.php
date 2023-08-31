b2977572f8857cb4cf799e17b6b82d726c28786d
Rifki Madhani <rifkimadhani@gmail.com>
2023-08-31 13:41:50 +0800

    fix sidebar toggle on small screen

--- app/Views/layout/scripts.php
+++ app/Views/layout/scripts.php
@@ -1,4 +1,5 @@
-<!-- <script src="<?//= base_url('plugin/codebaseadmin/js/codebase.core.min.js'); ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/codebaseadmin/js/codebase.core.min.js'); 
+                    ?>"></script> -->
 
 <!--
     Codebase JS
@@ -24,54 +25,78 @@
 <!-- Sync from old template -->
 
 <!-- Bootstrap -->
-<!-- <script src="<?//= base_url('plugin/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script> -->
-<!-- <script src="<?//= base_url('plugin/adminlte/bower_components/select2/dist/js/select2.full.min.js') ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js') 
+                    ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/bower_components/select2/dist/js/select2.full.min.js') 
+                    ?>"></script> -->
 <script src="<?= base_url('plugin/codebaseadmin/js/plugins/select2/js/select2.full.min.js') ?>"></script>
 <script src="<?= base_url('plugin/adminlte/bower_components/fastclick/lib/fastclick.js') ?>"></script>
-<!-- <script src="<?//= base_url('plugin/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js') 
+                    ?>"></script> -->
 <script src="<?= base_url('plugin/codebaseadmin/js/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
-<!-- <script src="<?//= base_url('plugin/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') 
+                    ?>"></script> -->
 <script src="<?= base_url('plugin/codebaseadmin/js/plugins/datatables/dataTables.bootstrap4.min.js') ?>"></script>
 <script src="<?= base_url('plugin/RowReorder-1.2.6/js/dataTables.rowReorder.min.js') ?>"></script>
 <script src="<?= base_url('plugin/adminlte/bower_components/inputmask/dist/jquery.inputmask.bundle.js') ?>"></script>
-<!-- <script src="<?//= base_url('plugin/adminlte/dist/js/adminlte.min.js') ?>"></script> -->
-<!-- <script src="<?//= base_url('plugin/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.js') ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/dist/js/adminlte.min.js') 
+                    ?>"></script> -->
+<!-- <script src="<? //= base_url('plugin/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.js') 
+                    ?>"></script> -->
 <script src="<?= base_url('plugin/codebaseadmin/js/plugins/jquery-slimscroll/jquery.slimscroll.js') ?>"></script>
 <script src="<?= base_url('plugin/js/datetostring.js'); ?>"></script>
 <script src="<?= base_url('plugin/js/uriquerystring.js'); ?>"></script>
-<!--<script src="--><?//= base_url('plugin/js/modalutil.js'); ?><!--"></script>-->
-<!--<script src="--><?//= base_url('plugin/js/videohls.js'); ?><!--" type="text/javascript"></script>-->
+<!--<script src="--><? //= base_url('plugin/js/modalutil.js'); 
+                    ?><!--"></script>-->
+<!--<script src="--><? //= base_url('plugin/js/videohls.js'); 
+                    ?><!--" type="text/javascript"></script>-->
 
 <!-- menu active -->
 <script>
     var baseurl = "<?php echo base_url() ?>";
     var url = window.location;
     var urlsplit = url.href.split('/');
-    var fullurl = urlsplit[0]+'/'+urlsplit[1]+'/'+urlsplit[2]+'/' + urlsplit[3]+'/'+urlsplit[4];
+    var fullurl = urlsplit[0] + '/' + urlsplit[1] + '/' + urlsplit[2] + '/' + urlsplit[3] + '/' + urlsplit[4];
     console.log(fullurl);
 
     $('ul.nav-main > li > a').filter(function() {
-    // console.log(this.href);
-    return this.href == fullurl;
+        // console.log(this.href);
+        return this.href == fullurl;
     }).siblings().removeClass('active').end().addClass('active');
 
     $('ul.nav-main > li > a.nav-submenu').filter(function() {
-    return $(this).parent().find('ul > li > a').filter(function() {
-        return this.href == fullurl;
-    }).length;
+        return $(this).parent().find('ul > li > a').filter(function() {
+            return this.href == fullurl;
+        }).length;
     }).parent().siblings().removeClass('open').end().addClass('open');
-    
+
     $('li.open > ul > li > a').filter(function() {
         return this.href == fullurl;
     }).siblings().removeClass('active').end().addClass('active');
 
     $('li.open > ul > li > a.nav-submenu').filter(function() {
-    return $(this).parent().find('ul > li > a').filter(function() {
-        return this.href == fullurl;
-    }).length;
+        return $(this).parent().find('ul > li > a').filter(function() {
+            return this.href == fullurl;
+        }).length;
     }).parent().siblings().removeClass('open').end().addClass('open');
 
     $('li.open > ul > li.open > ul > li > a').filter(function() {
         return this.href == fullurl;
     }).siblings().removeClass('active').end().addClass('active');
+</script>
+
+<!-- sidebar toggle button -->
+<script>
+    // Function to update the data-action attribute based on screen size
+    function updateDataAction() {
+        const btn = document.getElementById('sidebarToggleBtn');
+        const dataAction = window.innerWidth >= 992 ? 'sidebar_mini_toggle' : 'sidebar_toggle';
+        btn.setAttribute('data-action', dataAction);
+    }
+
+    // Initial call to set data-action based on initial screen size
+    updateDataAction();
+
+    // Add event listener to update data-action when the window is resized
+    window.addEventListener('resize', updateDataAction);
 </script>
