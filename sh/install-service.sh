92b96a9f56780502b4b158f204f488e6f0c9ed2d
Erick <frederick.christian@madeiraresearch.com>
2023-11-27 08:42:11 +0700

    modif script install service

--- sh/install-service.sh
+++ sh/install-service.sh
@@ -3,7 +3,19 @@
 #get path of this script
 WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+systemctl stop dispatcher
+systemctl stop xampp
+
 #copy service file from service folder to /etc/systemd/system folder
 cp $WD/../service/dispatcher.service /etc/systemd/system/.
 cp $WD/../service/xampp.service /etc/systemd/system/.
+
+systemctl disable dispatcher
+systemctl disable xampp
+
+systemctl enable dispatcher
+systemctl enable xampp
+
+systemctl start dispatcher
+systemctl start xampp
 
