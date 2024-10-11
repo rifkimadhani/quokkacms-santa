6922e846d685a1de3e5ab6852e8c8b2daffdd4ca
Erick <frederick.christian@madeiraresearch.com>
2024-10-11 11:18:58 +0700

    add version

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,9 +1,14 @@
+<?php
+require_once __DIR__ . '/../../../config/version.php';
+$version = VERSION;
+?>
+
 <!-- Footer -->
 <footer id="page-footer" class="opacity-0">
     <div class="content py-20 font-size-xs clearfix">
         <div class="float-right">
             <!-- Crafted with <i class="fa fa-heart text-pulse"></i> by <a class="font-w600" href="https://1.envato.market/ydb" target="_blank">pixelcave</a> -->
-            Version 1.0.1 - <?=date(DATE_RFC2822)?>
+            Version <?=$version?> - <?=date(DATE_RFC2822)?>
         </div>
         <div class="float-left">
           <strong>
---
+++ config/version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erick
+ * Date: 10/11/2024
+ * Time: 10:59
+ */
+
+define('VERSION', '1.0.20220101');
+
+/**
+ * History
+ *
+ * 1.0.20220101
+ *      - first version
+ *
+ */
