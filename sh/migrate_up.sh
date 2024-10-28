fa243ca9f4419d82ff96983b0b0c091319759c26
Erick <frederick.christian@madeiraresearch.com>
2024-10-28 10:56:12 +0700

    add parameter check utk migrate_up.sh

--- sh/migrate_up.sh
+++ sh/migrate_up.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+# Check if username and database parameters are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <username> <database>"
+    exit 1
+fi
+
 #mysql path
 path=/opt/lampp/bin/
 
@@ -8,3 +14,7 @@ database=$2
 
 #ttheme - add field last_update
 $path/mysql -u $username -p -e "USE ${database}; ALTER TABLE ttheme ADD last_update DATETIME DEFAULT CURRENT_TIMESTAMP;"
+
+#facility sorting
+# - tfacility - add field ord
+$path/mysql -u $username -p -e "USE ${database}; ALTER TABLE tfacility ADD ord INT DEFAULT 100;"
