f82bca39bdb34d66d0e51ffb008f412bbded8aac
Rifki Madhani <rifkimadhani@gmail.com>
2024-12-05 17:11:25 +0800

    fix ui table theme's detail - aligned table header & body

--- config/version.php
+++ config/version.php
@@ -6,11 +6,20 @@
  * Time: 10:59
  */
 
-define('VERSION', '1.0.20220101');
+define('VERSION', '1.1.20241205');
 
 /**
  * History
  *
+ * 1.1.20241205
+ *      - fix ui table theme's detail - aligned table header & body
+ *      - fix ui table facility & locality - aligned table header & body
+ *      - update Devices menu - sort by last_seen
+ *      - add feature Running Text
+ *      - fix Delete (constraint to tstb_session) on STBDevicesModel
+ *      - update profile pict
+ *      - add 2 second setelah connect utk menghindari device offline
+ * 
  * 1.0.20220101
  *      - first version
  *
